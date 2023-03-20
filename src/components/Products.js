15b5e8c8d79c14723f619033f04e87cd4f7fb5d1
Azzirr <arek.sliwowski37@gmail.com>
2023-03-20 13:01:09 +0100

    Response check

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,20 +6,19 @@ import { useEffect, useState } from 'react'
 export default function Products(props){
 
     const [products, setProducts] = useState(null)
-    // function productRender(event){
-    //     const value = event.target.value
-    //     console.log(products)
-    //     fetch(`https://fakestoreapi.com/products/${value}`)
-    //         .then(res => res.json())
-    //         .then(data => setProducts(data))
-    //         .catch(err => console.log(err))
-    // }
+
     useEffect(() => {
         fetch(`https://fakestoreapi.com/products/`)
-             .then(res => res.json())
+             .then(response => {
+                if(response.ok){
+                    return response.json()
+                } else {
+                    return Promise.reject(response)
+                }
+             })
              .then(data => {
                 console.log(data)
-                setProducts(data)
+                setProducts(data)                
             })
              .catch(err => console.log(err))
     }, []) 
@@ -29,7 +28,7 @@ export default function Products(props){
                 {products && <Row className="text-center ms-2 mt-4" variant="light">
                     {/* <input onChange={productRender} type="text" placeholder='input' ></input> */}
                     {products.map((product) => (
-                        <Col md={4}>
+                        <Col md={4} key={product.id}>
                             <img src={product.image} alt="product" width={200} height={200}></img>
                             <p>{product.title}</p>
                             <p>{product.price} $</p>
