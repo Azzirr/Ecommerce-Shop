ed2f10814e93645ce671aaff6e5ab120c88ae627
Azzirr <arek.sliwowski37@gmail.com>
2023-04-04 15:44:05 +0200

    Login form validation, implemented axios, authentication context

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,36 +1,127 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/esm/Container';
-import Col from 'react-bootstrap/esm/Col';
-import Row from 'react-bootstrap/esm/Row';
-export default function LoginForm(){
-    return(
-    <Container style={{float: "right"}}>
-        <Row>
-            <Col xs={12} sm={8} md={3} className="border border-success rounded loginFormStyle">
-                <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthenticationContext from "../context/AuthenticationContext";
+import axios from '../login api/axios';
+const LOGIN_URL = '/authentication';
+
+export default function LoginForm() {
+  const { setAuthentication } = useContext(AuthenticationContext);
+  const userRef = useRef();
+  const errorRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false); //in future this will change something in main page
+
+    useEffect(() => {
+      userRef.current.focus();
+    }, [])
+    useEffect(() => {
+      setErrorMessage('')
+    }, [user, password])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+        const response = await axios.post(LOGIN_URL, 
+            JSON.stringify({user, password}), {
+                headers: { 'Content-Type': 'application/json'},
+                withCredentials: true
+            }
+        );
+        console.log(JSON.stringify(response?.data));
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuthentication({user, password, roles, accessToken})
+        setUser("");
+        setPassword("");
+        setSuccess(true);
+    } catch (error) {
+        if(!error?.response){
+            setErrorMessage('No server response')
+        } else if (error.response?.status === 400) {
+            setErrorMessage('Missing username or password')
+        } else if (error.response?.status === 401) {
+            setErrorMessage('Unauthorized')
+        } else {
+            setErrorMessage('Login Failed')
+        }
+        errorRef.current.focus();
+    }
+
+
+  };
+
+  return (
+    <Container style={{ float: "right" }}>
+      <Row>
+        <Col
+          xs={12}
+          sm={8}
+          md={3}
+          className="border border-success rounded loginFormStyle"
+        >
+          <>
+            {success ? (
+              <Row>
+                <h1>You are logged in!</h1>
+              </Row>
+            ) : (
+              <Form onSubmit={handleSubmit}>
+                <Row>
+                  <p
+                    ref={errorRef}
+                    className={errorMessage ? "errormessage" : "offscreen"}
+                    aria-live="assertive"
+                  ></p>
+                </Row>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(event) => setUser(event.target.value)}
+                    value={user}
+                    required
+                  />
+                  <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
-                </Form.Text>
+                  </Form.Text>
                 </Form.Group>
-        
+
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter your password"
+                    onChange={(event) => setPassword(event.target.value)}
+                    value={password}
+                    required
+                  />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+                  <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="primary" type="submit" className="mb-1">
-                Login
+                  Login
                 </Button>
-                </Form>
-            </Col>
-        </Row>
+              </Form>
+            )}
+          </>
+        </Col>
+      </Row>
     </Container>
-
-    )
+  );
 }
